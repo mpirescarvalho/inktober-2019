946cd26ad7b1dab5b2c487154843bfab0ce29223
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2019-10-02 11:09:41 -0300

    Updates

---
+++ precache-manifest.84e89ebc009dde1bfb6b63c8be939db1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b800973402188542e9d3047472145c",
+    "url": "/inktober-2019/index.html"
+  },
+  {
+    "revision": "6eb43f2efdbdd4a890e6",
+    "url": "/inktober-2019/static/js/2.1e5b1b87.chunk.js"
+  },
+  {
+    "revision": "36b4cba14db8ede4173e",
+    "url": "/inktober-2019/static/js/main.214edf7d.chunk.js"
+  },
+  {
+    "revision": "fbc813efe12dfc7ff069",
+    "url": "/inktober-2019/static/js/runtime-main.0f5af469.js"
+  },
+  {
+    "revision": "f78a52a0c13d7128fd2d8c586e1ebf62",
+    "url": "/inktober-2019/static/media/dorians-pixel.f78a52a0.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/inktober-2019/precache-manifest.84e89ebc009dde1bfb6b63c8be939db1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/inktober-2019/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
